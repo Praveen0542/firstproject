0ac1d63a4a388d72e9ed51907be5a52180d182ff
praveen0524 <stanelypraveen007@gmail.com>
2023-01-13 20:04:58 +0530

    m1

---
+++ add.java
@@ -0,0 +1,10 @@
+class add{
+    public static void main(String args[]){
+        int a,b,c,d;
+        a=1;
+        b=3;
+        c=34;
+        d=45;
+        System.out.println(a+b+c+d);
+    }
+}
